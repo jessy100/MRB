d6c0b18408f7dc57cd7e85ad89240601267284a0
Alexander Freeman <Rubberduck313@gmail.com>
2016-05-12 07:46:13 +0200

    Alarm code for 1 minute movement check; not tested

--- AlarmSMS/src/movement.ino
+++ AlarmSMS/src/movement.ino
@@ -1,5 +1,6 @@
 unsigned long last_time = millis();
-bool still_triggered = false;
+unsigned long last_minute = millis();
+unsigned long last_alarm = millis();
 int counter = 0;
 
 void setup() {
@@ -12,21 +13,27 @@ void setup() {
 void loop() {
     if (!device_is_on()) {
         last_time = millis();
+
         return;
     }
+
+    if ((millis() - last_alarm) > 20000) { // 1 minuut
+        send_sms("No movement in last 10 seconds");
+    }
+
+    if ((millis() - last_time) > 60000) { // 1 minuut
+        counter = 0;
+        last_minute = millis();
+    }
     if ((millis() - last_time) > 2000) { // 2 seconds
-        bool alarm = last_two_seconds();(); //Tumblr in action x)
+        bool alarm = last_two_seconds(); //Tumblr in action x)
         if (alarm) {
-            if (!still_triggered) {
-                counter++;
-                still_triggered = true;
-                if (counter > 10) {
-                    send_sms();
-                }
+            last_alarm = millis();
+            counter++;
+            if (counter > 10) {
+                send_sms("10 movements in 1 minute!");
+                counter = 0;
             }
-        }
-        else {
-            still_triggered = true;
         }
         last_time = millis();
     }
@@ -44,6 +51,6 @@ bool last_two_seconds() {
   return analogRead(0) > 350;
 }
 
-void send_sms() {
+void send_sms(char text[]) {
     //SMS stuff here
 }
